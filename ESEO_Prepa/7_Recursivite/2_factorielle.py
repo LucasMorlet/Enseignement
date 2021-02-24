e27e960db7135e703487dc13811079467d3f1651
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-02-24 10:57:02 +0100

    Déplacement du dossier Prépa dans un dépôt à part

--- ESEO_Prepa/7_Recursivite/2_factorielle.py
+++
@@ -1,9 +0,0 @@
-def factorielle ( n ) :
-    if ( n <= 1 ) : 
-        return 1
-    else : 
-        return n*factorielle ( n-1 )
-        
-        
-for i in range (10) :
-    print ( i, "! =", factorielle(i) )
--- ESEO_Prepa/7_Recursivite/4_nombres_relatifs.py
+++
@@ -1,7 +0,0 @@
-def nombresRelatifs ( n ) :
-    print ( -n, end=" " )
-    if ( n > 0 ) :
-        nombresRelatifs ( n - 1 )
-        print ( n, end=" " )
-        
-nombresRelatifs ( 5 )
